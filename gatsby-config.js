98351429884e9a28ca32590b603dcef1c6dc49a2
Your Name <you@example.com>
2019-10-07 16:20:18 -0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
+    title: `The Musings of Zach Hurst`,
+    name: `Zach Hurst`,
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to the Musings of Zach Hurst, an experimental blog built atop a Gatsby JS theme called Novela by Narative.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/zachhurst`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/zachhurst`,
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/zhurst`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/in/zhurst/`,
       },
     ],
   },
